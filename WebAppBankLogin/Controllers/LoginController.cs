ef0fdcec79918a7b537c0f02d10c75251092a936
Niteshvarmaa <niteshvarma126@gmail.com>
2024-02-25 12:07:16 +0530

    final commit

---
+++ WebAppBankLogin/Controllers/LoginController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebAppBankLogin.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Login(string username, string password)
+        {
+
+            return RedirectToAction("Index", "Dashboard", new { username = username });
+        }
+    }
+}
+    
+
